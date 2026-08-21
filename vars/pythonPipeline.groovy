278d40e553ffcc3a7c7f7896b9696a7453358a72
unknown <brijesh.kori@amrutsoftware.com>
2026-08-21 16:47:27 +0530

    update code

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -1,7 +1,31 @@
 def call() {
 
-    stage('Hello') {
-        echo 'Running Python shared pipeline'
+    stage('Checkout') {
+        checkout scm
+    }
+
+    stage('Install Dependencies') {
+        sh '''
+            python3 -m venv venv
+            . venv/bin/activate
+
+            pip install -r requirements.txt
+            pip install build twine
+        '''
+    }
+
+    stage('Test') {
+        sh '''
+            . venv/bin/activate
+            PYTHONPATH=. pytest
+        '''
+    }
+
+    stage('Build Package') {
+        sh '''
+            . venv/bin/activate
+            python -m build
+        '''
     }
 
 }
